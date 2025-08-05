75e94d02fe18cea84816e13517f5ed1116479af6
Saurav <subedisaurav09@gmail.com>
2025-08-05 17:13:13 -0400

    ENV mode updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+// src/config.js
+export const API_BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "https://promtica.applikuapp.com/"
+    : "http://localhost:8000";
+
 export const IMAGE_BASE_URL = `${API_BASE_URL}`;
