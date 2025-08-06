4f2ff748361b9fa7f0b9cdfdbd925785b73bf1a0
Saurav <subedisaurav09@gmail.com>
2025-08-06 17:36:54 -0400

    server base url changed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 export const API_BASE_URL =
   import.meta.env.MODE === "production"
-    ? "https://prompticaserver.applikuapp.com/"
+    ? "https://server.promptica.chat/"
     : "http://localhost:8000";
 
 export const IMAGE_BASE_URL = `${API_BASE_URL}`;
